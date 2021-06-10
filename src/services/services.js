2fde7b3ca9aacd3093ee51df326c1cadec770a25
anprth <aanthony.praveen@gmail.com>
2021-06-10 20:18:01 +0530

    changed the new server endpoints

--- src/services/services.js
+++ src/services/services.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 export const getAllUniqueGarages = () =>
-  axios.get("http://20.197.28.152:8080/api/v1/getAllUniqueGarageLocations");
+  axios.get("http://157.245.105.114:8080/api/v1/getAllUniqueGarageLocations");
 
 export const getAllGarages = () =>
-  axios.get("http://20.197.28.152:8080/api/v1/getAllGarages");
+  axios.get("http://157.245.105.114:8080/api/v1/getAllGarages");
 
 export const getLocationByLocation = (location) =>
   axios.get(
-    `http://20.197.28.152:8080/api/v1/getGaragesOnLocation?location=${location}`
+    `http://157.245.105.114:8080/api/v1/getGaragesOnLocation?location=${location}`
   );
 
 export const getGaragesByName = (name) =>
   axios.get(
-    `http://20.197.28.152:8080/api/v1/getGarageDetailsByName?garageName=${name}`
+    `http://157.245.105.114:8080/api/v1/getGarageDetailsByName?garageName=${name}`
   );
 
 export const getAllGaragesUsingRegex = (name) =>
   axios.get(
-    `http://20.197.28.152:8080/api/v1/getAllGaragesUsingRegex?garageNameContaining=${name}`
+    `http://157.245.105.114:8080/api/v1/getAllGaragesUsingRegex?garageNameContaining=${name}`
   );
 
 export const getAllGaragesByLatAndLong = (distance, latitude, longitude) =>
   axios.get(
-    `http://20.197.28.152:8080/api/v1/getGaragesByLatAndLong?distanceInKms=${distance}&latitude=${latitude}&longitude=${longitude}`
+    `http://157.245.105.114:8080/api/v1/getGaragesByLatAndLong?distanceInKms=${distance}&latitude=${latitude}&longitude=${longitude}`
   );
