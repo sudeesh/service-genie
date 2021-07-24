33503f7bbd845b97c5af38e5e3b59d41ace13483
sudeesh <sudeesh.b26@gmail.com>
2021-07-24 20:10:39 +0100

    map changes for testing

--- src/components/map/map.component.js
+++ src/components/map/map.component.js
@@ -89,7 +89,7 @@ export class MapContainer extends Component {
         google={this.props.google}
         zoom={14}
         style={mapStyles}
-        // initialCenter={{ lat: this.state.latitude, lng: this.state.longitude }}
+        initialCenter={{ lat: this.state.latitude, lng: this.state.longitude }}
         // center={{ lat: this.state.latitude, lng: this.state.longitude }}
         className="map-holder"
         // onMarkerDragEnd={(props, mapProps) => this.test(props, mapProps)}
