f0bd50c724690deadc1aab14aaea1c75fcef49c2
sudeesh <sudeesh.b26@gmail.com>
2021-08-08 17:48:34 +0100

    files for cookies

--- src/pages/searchPage/searchPage.component.js
+++ src/pages/searchPage/searchPage.component.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
 // components
 import Search from "../../components/search/search.component";
@@ -8,6 +9,16 @@ const SearchPage = (props) => {
   const { latitude, longitude, history } = props;
   return (
     <>
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept Cookies"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
       <Search />
       <Map latitude={latitude} longitude={longitude} history={history} />
     </>
