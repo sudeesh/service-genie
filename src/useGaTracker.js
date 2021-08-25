6e69fc1eb66ffd16d44a4d9b819f79e34dc083df
sudeesh <sudeesh.b26@gmail.com>
2021-08-25 06:25:48 +0100

    google analytics

--- src/useGaTracker.js
+++ src/useGaTracker.js
@@ -7,9 +7,7 @@ const useGaTracker = () => {
   const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    if (!window.location.href.includes("localhost")) {
-      ReactGA.initialize("UA-205741741-1");
-    }
+    ReactGA.initialize("UA-205741741-1");
     setInitialized(true);
   }, []);
 
